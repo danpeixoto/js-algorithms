4697be1eeaa41eaf31f775438634c122a547a1c5
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2023-02-20 17:23:29 -0300

    feat: add sentinel search

---
+++ search/sentinel-search/index.js
@@ -0,0 +1,19 @@
+const { generateNumbers } = require('../utils/index');
+
+const SEARCH_THIS = 42;
+console.log('We will search this number', { number: SEARCH_THIS });
+
+const numberArray = generateNumbers(__dirname);
+numberArray.push(SEARCH_THIS);
+
+let i = 0;
+
+while (numberArray[i] !== SEARCH_THIS) {
+  i++;
+}
+
+if (i === numberArray.length - 1) {
+  console.log('Number was not found');
+} else {
+  console.log('Number was found at index: ', i);
+}
--- search/utils/index.js
+++ search/utils/index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 
-const generateNumbers = (dirname, maxNumbers = 1000) => {
-  const numberArray = [...Array(maxNumbers)].map(e => Math.floor(Math.random() * maxNumbers));
+const generateNumbers = (dirname, maxNumbers = 1000, maxValue = 1000) => {
+  const numberArray = [...Array(maxNumbers)].map(e => Math.floor(Math.random() * maxValue));
 
   fs.writeFileSync(`${dirname}/numbers.txt`, numberArray.toString());
   console.log('This is the array that will be used:', { array: numberArray });
